b9dc8a4d12dbc3f7ee5c2c8509623500dce8d2a0
Binay <bbmcloud16@gmail.com>
2026-06-12 18:04:34 +0800

    fix Git Action build error-fix compilation error

--- src/test/java/com/library/LibrarySystemApplicationTests.java
+++ src/test/java/com/library/LibrarySystemApplicationTests.java
@@ -2,7 +2,7 @@ package com.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.jupiter.api.Disabled;
 @Disabled
 @SpringBootTest
 class LibrarySystemApplicationTests {
